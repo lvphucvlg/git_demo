4e470fbc4178e0439565c87cccecfa0402e969fb
phuclv.vlg <phuclv.vlg@vnpt.vn>
2021-12-16 19:01:12 +0700

    dev

--- controller.php
+++ controller.php
@@ -10,24 +10,5 @@ use Yajra\DataTables\Facades\DataTables;
 
 class DanhGiaCovidController extends Controller
 {
-    public function index(Request $request)
-    {
-        if ($request->ajax()) {
-            $filterParams = \Arr::except($request->all(), array('_token', '_method'));
 
-            if (!empty($filterParams)) {
-                $startDate = $filterParams['start_date'];
-                $endDate = $filterParams['end_date'];
-                $data = danhgiacovid::getReport($startDate, $endDate);
-            }
-
-            return DataTables::of($data)
-                ->addColumn('chitiet', function ($row) {
-                    return "<a class='btn btn-sm btn-primary' href='/baocao/danhgiacovid/{$row->khaosat_id}'><i class='fas fa-arrow-circle-right'></i></a>";
-                })
-                ->rawColumns(['chitiet'])->make(true);
-        }
-
-        return view('pages.baocao.danhgiacovid');
-    }
 }
